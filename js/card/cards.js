6db94bd8ca2d17251da0a2f4dfca8828583f52d4
alvadia <vadim1221@hotmail.com>
2020-08-15 10:37:20 +0300

    background + colors

--- js/card/cards.js
+++ js/card/cards.js
@@ -1,24 +1,6 @@
 
 var Card = React.createClass({
   render: function() {
-    var typeClassIndex = {
-      "Волшебник": "cardArcane",
-      "Колдун": "cardKoldun",
-      "Чародей": "cardSorserer",
-      "Следопыт": "cardRanger",
-      "Жрец": "cardCleric",
-      "Жрец Бури" : "cardCleric",
-      "Жрец Войны":"cardCleric",
-      "Жрец Жизни":"cardCleric",
-      "Жрец Знания":"cardCleric",
-      "Жрец Обмана":"cardCleric",
-      "Жрец Природы":"cardCleric",
-      "Жрец Света":"cardCleric",
-      "Друид": "cardDruid",
-      "Паладин": "cardPaladin",
-      "Бард": "cardBard"
-    };
-    var typeClass = typeClassIndex[this.props.type];
     var bigNameLength = this.props.data.name.length;
     for (var i = 0; i <bigNameLength; i++) {
       if (~['Ж','Ш','Щ','М','ж','ш','щ','м'].indexOf(this.props.data.name[i])) {
@@ -26,10 +8,23 @@ var Card = React.createClass({
       }
     } 
     var bigName = bigNameLength > 26 ? true : false;
-    var cardClass = 'card ' + typeClass
+    var cardClass = 'card ';
+    var font = this.props.data.font || 2;
+    var level = this.props.data.level || 'Заговор';
+    var selfClass = this.props.data.class || 'Умение';
+    var name = this.props.data.name || 'Шаблон';
+    var color = this.props.color;
+    var style = {"backgroundColor": color};
+    var border = {
+            "borderBottomColor" : color,
+            "borderTopColor" : color,
+            "borderRightColor" : color,
+            "borderLeftColor" : color,
+            "borderColor" : color,
+    };
     if (this.props.showBack) {
       cardClass += ' back';
-      var lvlNum = this.props.data.level == 'Заговор' ? 0 : this.props.data.level.substr(0,1);
+      var lvlNum = level.substr(0,1);
       return (
         <div className={cardClass}>
           <div className='number'>{lvlNum}</div>
@@ -37,34 +32,55 @@ var Card = React.createClass({
         </div>
       );
     }
-    else return (
-      <div className={cardClass}>
-        <div className="title"><span className="fs">{this.props.data.name}</span></div>
-        <div className="level">{this.props.data.level}, {this.props.data.type}</div>
-        <div className="props">
-            <div className="prop">
-              <div className="propName">Время накладывания</div>
-              <div className="propValue">{this.props.data.time}</div>
-            </div>
-            <div className="prop">
-              <div className="propName">Дистанция</div>
-              <div className="propValue">{this.props.data.range}</div>
-            </div>
-            <div className="prop">
-              <div className="propName">Компоненты</div>
-              <div className="propValue">{this.props.data.components}</div>
-            </div>
-            <div className="prop">
-              <div className="propName">Длительность</div>
-              <div className="propValue">{this.props.data.duration}</div>
-            </div>
-        </div>
-        <CardText text={this.props.data.text} hightlevel={this.props.data.hightlevel} bigName={bigName}/>
+    else {
+    if (selfClass=="Умение") return (
+      <div className={cardClass + selfClass} style={style}>
+        <div className="title"><span className="fs">{name}</span></div>
+          <div className="text" style={{"height":"71mm"}}>
+            {
+              this.props.data.text.map(function(pText, ind) {
+              return (
+                <p className="txt" key={ind} style={{"fontSize": font + "mm", "lineHeight" : font * 0.9 + "mm"}}>{pText}</p>
+              );
+              }, this)
+            }
+          </div>
         <div className="footer">
-          {this.props.type} - {this.props.data.level}
+          {level}
         </div>
       </div>
     );
+    else {
+      return (
+        <div className="card" style={style}>
+          <div className="title"><span className="fs">{name}</span></div>
+          <div className="level" style={style}>{this.props.data.type}</div>
+          <div className="props">
+              <div className="prop" style={border}>
+                <div className="propName">Время</div>
+                <div className="propValue">{this.props.data.time}</div>
+              </div>
+              <div className="prop" style={border}>
+                <div className="propName">Дистанция</div>
+                <div className="propValue">{this.props.data.range}</div>
+              </div>
+              <div className="prop" style={border}>
+                <div className="propName">Компоненты</div>
+                <div className="propValue">{this.props.data.components}</div>
+              </div>
+              <div className="prop" style={border}>
+                <div className="propName">Длительность</div>
+                <div className="propValue">{this.props.data.duration}</div>
+              </div>
+          </div>
+          <CardText text={this.props.data.text} hightlevel={this.props.data.hightlevel} font={font} color={color}/>
+          <div className="footer">
+            {level}
+          </div>
+        </div>
+        );
+      }
+    }
   }
 });
 
@@ -72,50 +88,20 @@ var CardText = React.createClass({
   render: function() {
     var fontSizeClass = 'text';
     var len = 0;
-    this.props.text.forEach(function(pText, ind) {
-      len += pText.length;
-    })
-
-    if (this.props.bigName) {
-      if (len > 900) {
-        if (len > 1000) {
-          fontSizeClass += ' small';
-        }
-        else {
-          fontSizeClass += ' preSmall'
-        }
-      }
-    } else {
-      // if (len > 1150) {
-        if (len > 1250) {
-          fontSizeClass += ' small';
-        }
-        else {
-          // fontSizeClass += ' preSmall'
-        }
-      // }
-    }
-
-    // if ((len > 1000 && this.props.bigName) || len > 1250) {
-    //     fontSizeClass += ' small';
-    // }
-    if (this.props.bigName) {
-      fontSizeClass += ' bigName';
-    }
-
+    var color = this.props.color;
+    var font = this.props.font;
     var cardText = this.props.text.map(function(pText, ind) {
-      var pClass = ind == 0 ? "first" : "";
       return (
-        <p className={pClass}>{pText}</p>
+        <p className="txt" key={ind} style={{"fontSize": font + "mm", "lineHeight" : font - 0.3 + "mm"}}>{pText}</p>
       );
     });
     var higherLevels = "";
     if (this.props.hightlevel && this.props.hightlevel.length) {
-      higherLevels = (<HigherLevels text={this.props.hightlevel} />);
+      higherLevels = (<HigherLevels text={this.props.hightlevel} color={color}/>);
     }
     return (
       <div className={fontSizeClass}>
-        <div className="level"></div>
+        <div className="level" style={{"backgroundColor": color}}></div>
         {cardText}
         {higherLevels}
       </div>
@@ -126,23 +112,23 @@ var CardText = React.createClass({
 
 var HigherLevels = React.createClass({
   render: function() {
-    var cardText = this.props.text.map(function(pText, ind) {
-      var pClass = ind == 0 ? "first" : "";
-      return (
-        <p className={pClass}>{pText}</p>
-      );
-    });
+    var color = this.props.color;
     return (
       <div className="higherLevels">
-        <div className="level">На более высоком уровне</div>
-        {cardText}
+        <div className="level" style={{"backgroundColor": color}}>На более высоком уровне</div>
+        {
+          this.props.text.map(function(pText, ind) {
+            return (
+              <p className="txt" key={ind}>{pText}</p>
+            );
+          })
+        }
       </div>
     );
   }
 });
 
 var Cards = React.createClass({
-  classesCard : {},
   cards : [],
   loadCommentsFromServer: function() {
     $.ajax({
@@ -151,10 +137,8 @@ var Cards = React.createClass({
       cache: false,
       success: function(data) {
         var text = data.replace(/\r\n/g,' ')
-        var arr = JSON.parse(text);
-        this.classesCard = arr.classesCard;
-        this.cards = arr.cards;
-        this.setState({selectedClass: 'Все'});
+        this.cards = JSON.parse(text).cards;
+        this.setState({});
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -162,92 +146,28 @@ var Cards = React.createClass({
     });
   },
   getInitialState: function() {
-    return {data: [], selectedClass: 'Все', showBacks: false, 
-    lvl0: false,
-    lvl1: false,
-    lvl2: false,
-    lvl3: false,
-    lvl4: false,
-    lvl5: false,
-    lvl6: false,
-    lvl7: false,
-    lvl8: false,
-    lvl9: false};
+    return {data: [], showBacks: false};
   },
   componentDidMount: function() {
     this.loadCommentsFromServer();
-    // setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-  },
-  handleChange: function(event) {
-    this.setState({selectedClass: event.target.value});
-  },
-  handleChangeLvl: function(lvl, event) {
-    var lvlName = 'lvl'+lvl;
-    var obj = {};
-    obj[lvlName] = !this.state[lvlName];
-    this.setState(obj);
-  },
-  showBacksChange: function(event) {
-    this.setState({showBacks: !this.state.showBacks  });
   },
   render: function() {
-    var cardNodes = '';
-    if (this.state.selectedClass == 'Все') {
-      cardNodes = this.cards.map(function(card) {
-        return (
-          <Card data={card} type={this.state.selectedClass} showBack={this.state.showBacks}/>
-          );
-        }, this);
-    } else {
-      var arr = [];
-      this.cards.forEach(function(card) {
-        var lvlNum = card.level == 'Заговор' ? 0 : card.level.substr(0,1);
-        var lvlName = 'lvl' + lvlNum;
-        if (this.state[lvlName] && ~this.classesCard[this.state.selectedClass].indexOf(card.name )) {
-          arr.push(card)
-        } 
-        else {
-        }
-      }, this)
-
-      arr.sort(function(self) {
-        return function(a,b) {
-          return self.classesCard[self.state.selectedClass].indexOf(a.name) - self.classesCard[self.state.selectedClass].indexOf(b.name) ;
-        }
-      }(this))
-
-      
-      if (this.state.showBacks) {
-        var tempArr = [];
-        while (arr.length) {
-          var slice = arr.splice(0,3);
-          for (var i = slice.length - 1; i >= 0; i--) {
-            tempArr.push(slice[i])
-          }
-        }
-        arr = tempArr;
-      console.log(this.state.showBacks)
-      }
-      cardNodes = arr.map(function(card) {
-        return (
-          <Card data={card} type={this.state.selectedClass} showBack={this.state.showBacks}/>
-          );
-        }, this);
-    }
-    // var classType = {""}[this.state.type]
-
+    var color = this.props.color;
     return (
-      <div>
-        <div className="cards" >
-          {cardNodes}
+        <div className="cards" key="cards">
+          {
+            this.cards.map(function(card, ind)
+              {
+              return(<Card data={card} showBack={this.state.showBacks} key={ind} color={color}/>);
+              }, this)
+          }
         </div>
-      </div>
     );
   }
 });
 
 
 ReactDOM.render(
-  <Cards url="api/cards.json" pollInterval={5000} type="Бард"/>,
+  <Cards url={document.getElementById("cards_builder").dataset.json} pollInterval={5000} color={document.getElementById("cards_builder").dataset.color} />,
   document.getElementById('content')
 );
